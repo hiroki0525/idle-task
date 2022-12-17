834865729d04c8e6ea8dbaf2fdcfd57bae4d00b1
Hiroki Miyaji <hdekm852@gmail.com>
2022-12-17 12:58:21 +0900

    feat: add support umd for CDN

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,16 +2,19 @@ import typescript from "@rollup/plugin-typescript";
 import packageJson from "./package.json" assert { type: 'json' };
 import terser from '@rollup/plugin-terser';
 
+const buildConfig = ({ format, file }) => ({
+  file,
+  format,
+  sourcemap: true,
+  name: 'idleTask',
+  compact: true,
+})
+
 export default {
   input: "src/index.ts",
   output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-      name: "lib",
-      compact: true,
-    },
+      buildConfig({ file: packageJson.main, format: 'cjs' }),
+      buildConfig({ file: 'dist/umd/index.js', format: 'umd' })
   ],
   plugins: [typescript(), terser()],
 };
