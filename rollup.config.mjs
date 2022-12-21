97ac608e7469beb0e44f62bb5a248d698108d6a9
Hiroki Miyaji <hdekm852@gmail.com>
2022-12-21 14:14:46 +0900

    feat: support ESM and development packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,57 @@
 import typescript from '@rollup/plugin-typescript';
-import packageJson from './package.json' assert { type: 'json' };
 import terser from '@rollup/plugin-terser';
 import dts from 'rollup-plugin-dts';
 import tsConfigJson from './tsconfig.json' assert { type: 'json' };
 
-const buildConfig = ({ format, file }) => ({
-  file,
-  format,
-  sourcemap: true,
-  name: 'idleTask',
-  compact: true,
-});
+const outputFormats = ['cjs', 'umd', 'es'];
+
+const buildTsConfig = ({ format, sourceMap }) => {
+  const extraConfig =
+    format === 'umd'
+      ? { compilerOptions: { target: 'es5', sourceMap } }
+      : { compilerOptions: { sourceMap } };
+  return typescript({ ...tsConfigJson, ...extraConfig });
+};
+
+const productionConfigs = outputFormats.map(format => ({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: `dist/${format}/index.js`,
+      format,
+      sourcemap: tsConfigJson.compilerOptions.sourceMap,
+      name: 'idleTask',
+      compact: true,
+    },
+  ],
+  plugins: [
+    buildTsConfig({
+      format,
+      sourceMap: tsConfigJson.compilerOptions.sourceMap,
+    }),
+    terser(),
+  ],
+}));
+
+const developmentConfigs = outputFormats.map(format => ({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: `dist/${format}/index.development.js`,
+      format,
+      name: 'idleTask',
+    },
+  ],
+  plugins: [buildTsConfig({ format, sourceMap: false })],
+}));
 
 export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      buildConfig({ file: packageJson.main, format: 'cjs' }),
-    ],
-    plugins: [typescript(), terser()],
-  },
-  {
-    input: 'src/index.ts',
-    output: [
-      buildConfig({ file: 'dist/umd/index.js', format: 'umd' }),
-    ],
-    plugins: [typescript({...tsConfigJson, ...{ compilerOptions: {target: "es5"}}}), terser()],
-  },
+  ...productionConfigs,
+  ...developmentConfigs,
   // generate single file.
   // after build, delete all .d.ts files without index.d.ts
   {
-    input: './dist/index.d.ts',
+    input: './dist/es/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [dts()],
   },
