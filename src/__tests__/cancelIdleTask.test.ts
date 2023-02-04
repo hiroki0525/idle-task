0b4f9806b01be30ed566ef2747a54737450dbcf8
Hiroki Miyaji <hdekm852@gmail.com>
2023-02-04 23:39:06 +0900

    fix: not working `cancelIdleTask` when setting `SetIdleTaskOptions.revalidateInterval`

--- src/__tests__/cancelIdleTask.test.ts
+++ src/__tests__/cancelIdleTask.test.ts
@@ -109,30 +109,67 @@ describe('cancelIdleTask', () => {
   });
 
   describe('not existed tasks', () => {
-    beforeEach(() => {
-      taskId = idleTaskModule!.setIdleTask(createTask(mockFirstTask));
-      runRequestIdleCallback();
-      idleTaskModule!.setIdleTask(createTask(mockSecondTask));
-      idleTaskModule!.setIdleTask(createTask(mockThirdTask));
-      idleTaskModule!.cancelIdleTask(taskId);
+    describe('with SetIdleTaskOption.revalidateInterval', () => {
+      beforeEach(() => {
+        taskId = idleTaskModule!.setIdleTask(createTask(mockFirstTask), {
+          revalidateInterval: 1,
+        });
+        idleTaskModule!.setIdleTask(createTask(mockSecondTask));
+        idleTaskModule!.setIdleTask(createTask(mockThirdTask));
+        // one mockFirstTask will be remaining.
+        runRequestIdleCallback();
+        idleTaskModule!.cancelIdleTask(taskId);
+        // One remaining mockFirstTask will be executed.
+        runRequestIdleCallback();
+        // check not to work revalidateInterval option.
+        runRequestIdleCallback();
+      });
+
+      it('first task is called twice', () => {
+        expect(mockFirstTask.mock.calls.length).toBe(2);
+      });
+
+      it('second task is called', () => {
+        expect(mockSecondTask.mock.calls.length).toBe(1);
+      });
+
+      it('third task is not called', () => {
+        expect(mockThirdTask.mock.calls.length).toBe(1);
+      });
+
+      it('first task result is cleared', async () => {
+        await expect(
+          idleTaskModule!.waitForIdleTask(taskId)
+        ).resolves.toBeUndefined();
+      });
     });
 
-    it('first task is called', () => {
-      expect(mockFirstTask.mock.calls.length).toBe(1);
-    });
+    describe('without SetIdleTaskOption.revalidateInterval', () => {
+      beforeEach(() => {
+        taskId = idleTaskModule!.setIdleTask(createTask(mockFirstTask));
+        runRequestIdleCallback();
+        idleTaskModule!.setIdleTask(createTask(mockSecondTask));
+        idleTaskModule!.setIdleTask(createTask(mockThirdTask));
+        idleTaskModule!.cancelIdleTask(taskId);
+      });
 
-    it('second task is not called', () => {
-      expect(mockSecondTask.mock.calls.length).toBe(0);
-    });
+      it('first task is called', () => {
+        expect(mockFirstTask.mock.calls.length).toBe(1);
+      });
 
-    it('third task is not called', () => {
-      expect(mockThirdTask.mock.calls.length).toBe(0);
-    });
+      it('second task is not called', () => {
+        expect(mockSecondTask.mock.calls.length).toBe(0);
+      });
 
-    it('first task result is cleared', async () => {
-      await expect(
-        idleTaskModule!.waitForIdleTask(taskId)
-      ).resolves.toBeUndefined();
+      it('third task is not called', () => {
+        expect(mockThirdTask.mock.calls.length).toBe(0);
+      });
+
+      it('first task result is cleared', async () => {
+        await expect(
+          idleTaskModule!.waitForIdleTask(taskId)
+        ).resolves.toBeUndefined();
+      });
     });
   });
 });
