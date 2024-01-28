8ff33c81da28acb53cabd197d1fbd5664a26b617
Hiroki Miyaji <hdekm852@gmail.com>
2024-01-28 13:23:25 +0900

    perf: change build system rollup to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, Options } from 'tsup';
+import { cp } from 'fs/promises';
+
+export default defineConfig(options => {
+  const commonOptions: Partial<Options> = {
+    entry: ['src/index.ts'],
+    sourcemap: true,
+    dts: true,
+    clean: true,
+    outDir: './dist/',
+    ...options,
+  };
+
+  return [
+    // Development
+    {
+      ...commonOptions,
+      format: 'esm',
+      outExtension: () => ({ js: '.development.mjs' }),
+      onSuccess: async () => {
+        await cp('dist/index.development.mjs', 'dist/index.development.js');
+      },
+    },
+    {
+      ...commonOptions,
+      format: 'cjs',
+      outExtension: () => ({ js: '.development.cjs' }),
+    },
+    // Production
+    {
+      ...commonOptions,
+      format: 'esm',
+      outExtension: () => ({ js: '.mjs' }),
+      minify: true,
+      onSuccess: async () => {
+        await cp('dist/index.mjs', 'dist/index.js');
+      },
+    },
+    {
+      ...commonOptions,
+      format: 'cjs',
+      minify: true,
+      outExtension: () => ({ js: '.cjs' }),
+    },
+  ];
+});
